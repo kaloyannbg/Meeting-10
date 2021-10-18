e83711865f75a3ad14e85cdea62ffd764652191d
Kaloyan Angelov <75050098+kaloyannbg@users.noreply.github.com>
2021-10-19 00:21:14 +0300

    Create task3_remake.c

---
+++ task3_remake.c
@@ -0,0 +1,109 @@
+/*
+Упражнение 3.* Добавете проверка във функцията int atof(char
+*s) дали подаденият стринг няма показни позиции преди числото,
+и ако има, ги премахнете. Проверете дали числото не е
+отрицателно и, ако е така, включете знака в резултата
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+double atof(char *str);
+int isLetter(char c);
+
+int main()
+{
+
+    char strToDouble[] = "asdasd-25.2asdsa52-asdds";
+
+    //atof(strToDouble);
+
+    printf("%f", atof(strToDouble));
+
+    return 0;
+}
+
+int isLetter(char c)
+{
+
+    int isLet = (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ? 1 : 0;
+
+    return isLet;
+}
+
+double atof(char *str)
+{
+    // Remove letters, and 123456 or increment positions, if have '-'
+    char newArray[100];
+
+    int strLength = strlen(str);
+
+    int counterNewArray = 0;
+
+    int isNegative = 0;
+
+    for (int i = 0; i < strLength; i++)
+    {
+        if (str[i] == '-')
+        {
+            isNegative = 1;
+            continue;
+        }
+        else if (str[i] == i + '0')
+        {
+            continue;
+        }
+        else if (isLetter(str[i]))
+        {
+            continue;
+        }
+
+        newArray[counterNewArray] = str[i];
+        counterNewArray++;
+    }
+
+    newArray[counterNewArray] = '\0';
+
+    int place = 1;
+
+    double total = 0;
+
+    int digit = 0;
+
+    int pointNum = 0;
+
+    for (int i = counterNewArray - 1; i >= 0; i--)
+    {
+        if (newArray[i] == '.')
+        {
+            int temp = i;
+            while (temp >= 0)
+            {
+                pointNum++;
+                temp--;
+            }
+        }
+        else if (newArray[i] != '.')
+        {
+            digit = newArray[i] - 48;
+            total = total + (digit * place);
+            place = place * 10;
+        }
+    }
+
+    int divisor = 1;
+
+    for (int i = 0; i < pointNum; i++)
+    {
+        divisor = divisor * 10;
+    }
+
+    total = total / divisor;
+
+    if (isNegative)
+    {
+        total = total - (total * 2);
+        return total;
+    }
+    return total;
+}
